e27f5bbfa769db69ebd5d28c05151ec278c1040d
Alfredo Natal <anatal31@gmail.com>
2022-12-04 10:47:58 -0800

    fixed positioning of stripe button

--- components/CheckoutForm.jsx
+++ components/CheckoutForm.jsx
@@ -2,17 +2,15 @@ import React, { useEffect, useState } from "react";
 import { PaymentElement, useStripe, useElements, AddressElement } from "@stripe/react-stripe-js";
 import { getBaseUrl } from "nextjs-url";
 
-export default function CheckoutForm({amount, description}) {
+export default function CheckoutForm({ amount, description }) {
   const stripe = useStripe();
   const elements = useElements();
   const [message, setMessage] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [enabled, setEnabled] = useState(false);
-  const baseUrl = getBaseUrl().href; 
-
+  const baseUrl = getBaseUrl().href;
 
   useEffect(() => {
-
     if (!stripe) {
       return;
     }
@@ -41,7 +39,6 @@ export default function CheckoutForm({amount, description}) {
     });
   }, [stripe]);
 
-
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -55,7 +52,7 @@ export default function CheckoutForm({amount, description}) {
       elements,
       confirmParams: {
         return_url: `${baseUrl}/thankyou`,
-      }
+      },
     });
 
     // This point will only be reached if there is an immediate error when confirming the payment.
@@ -70,21 +67,21 @@ export default function CheckoutForm({amount, description}) {
 
   const paymentElementOptions = {
     layout: "tabs",
-    paymentMethodOrder: ['card', 'apple_pay', 'google_pay', ]
+    paymentMethodOrder: ["card", "apple_pay", "google_pay"],
   };
 
   return (
     <form className="w-screen sm:w-[512px] max-w-lg  p-5 rounded-lg overflow-y" onSubmit={handleSubmit}>
       {enabled ? (
-              <div className=" bg-[#2D3139] my-5 p-3 rounded-lg  divide-y divide-yellow-500 space-y-2">
-              <div>
-                <h1 className="text-base font-bold sm:font-normal sm:text-lg">{description ? 'Payment for ' + description : 'No services selected...'} </h1>
-              </div>
-              <div className="flex items-center justify-between pt-2">
-                <h1 className="text-base">{amount && description ? `Total: $${amount}` : "Total: $0.00"}</h1>
-                <h1 className="text-xs">{amount && description ? `Payable to ${process.env.NEXT_PUBLIC_USER}` : null}</h1>
-              </div>
-            </div>
+        <div className=" bg-[#2D3139] my-5 p-3 rounded-lg  divide-y divide-yellow-500 space-y-2">
+          <div>
+            <h1 className="text-base font-bold sm:font-normal sm:text-lg">{description ? "Payment for " + description : "No services selected..."} </h1>
+          </div>
+          <div className="flex items-center justify-between pt-2">
+            <h1 className="text-base">{amount && description ? `Total: $${amount}` : "Total: $0.00"}</h1>
+            <h1 className="text-xs">{amount && description ? `Payable to ${process.env.NEXT_PUBLIC_USER}` : null}</h1>
+          </div>
+        </div>
       ) : null}
 
       {/* <AddressElement options={{ mode: "billing" }} /> */}
@@ -94,14 +91,16 @@ export default function CheckoutForm({amount, description}) {
       <div className="relative text-red-500 italic top-2 text-sm">{message && <span>{message}</span>}</div>
 
       {enabled ? (
-        <button className="bg-yellow-500 text-white font-bold  h-[40px] w-full mt-4 rounded-lg" disabled={isLoading || !stripe || !elements}>
-          <span id="button-text">{isLoading ? <div className="spinner-border animate-spin inline-block w-5 h-5 border-4 rounded-full" /> : "Pay now"}</span>
-        </button>
+        <>
+          <button className="bg-yellow-500 text-white font-bold  h-[40px] w-full mt-4 rounded-lg" disabled={isLoading || !stripe || !elements}>
+            <span id="button-text">{isLoading ? <div className="spinner-border animate-spin inline-block w-5 h-5 border-4 rounded-full" /> : "Pay now"}</span>
+          </button>
+          <div className="text-xs pt-3 float-right flex space-x-[0.35rem]">
+            <p className=" ">Powered By</p>
+            <img src="./img/stripe.png" alt="" className="h-4" />
+          </div>
+        </>
       ) : null}
-      <div className="text-xs pt-3 float-right flex space-x-[0.35rem]">
-        <p className=" ">Powered By</p>
-        <img src="./img/stripe.png" alt="" className="h-4"/>
-      </div>
     </form>
   );
 }
