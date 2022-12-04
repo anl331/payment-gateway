16bae0f705cd7bd24f66611a5e8e4ddb19862d0e
Alfredo Natal <anatal31@gmail.com>
2022-12-04 11:50:32 -0800

    changed size of lotti animation

--- pages/thankyou.js
+++ pages/thankyou.js
@@ -15,7 +15,7 @@ function Thankyou() {
       autoplay
       keepLastFrame
       src="https://assets4.lottiefiles.com/packages/lf20_VKF83wmv1k.json"
-      style={{ height: '600px', width: '600px' }}
+      style={{ height: '550px', width: '550px' }}
     ></Player>
   </div>;
 }
